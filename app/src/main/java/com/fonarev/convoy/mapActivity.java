8b5d195521e50b8c8535448968207e9e9d72e9dc
Vichka Fonarev <fonarev.vichka@gmail.com>
2017-01-01 19:52:41 -0500

    get location with using google play apis, asks for permissions nicely, proof of concept done

--- app/src/main/java/com/fonarev/convoy/mapActivity.java
+++ app/src/main/java/com/fonarev/convoy/mapActivity.java
@@ -1,233 +1,254 @@
 package com.fonarev.convoy;
 
-import android.content.IntentSender;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.icu.text.DateFormat;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.ResultReceiver;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResult;
-import com.google.android.gms.location.LocationSettingsStatusCodes;
+public class mapActivity extends AppCompatActivity {
+    LocationManager mLocationManager;
+    double longitudeBest, latitudeBest;
+    double longitudeGPS, latitudeGPS;
+    double longitudeNetwork, latitudeNetwork;
+    TextView longitudeValueBest, latitudeValueBest;
+    TextView longitudeValueGPS, latitudeValueGPS;
+    TextView longitudeValueNetwork, latitudeValueNetwork;
 
-import java.util.Date;
-
-public class mapActivity extends AppCompatActivity implements
-        GoogleApiClient.OnConnectionFailedListener,
-        GoogleApiClient.ConnectionCallbacks,
-        ResultCallback<LocationSettingsResult>,
-        com.google.android.gms.location.LocationListener {
-
-    private GoogleApiClient mGoogleApiClient = null;
-    private Location mLastLocation, mCurrentLocation;
-    protected TextView mLatitudeText, mLongitudeText;
-    protected LocationSettingsRequest mLocationSettingsRequest;
-    protected LocationRequest mLocationRequest;
-
-    protected TextView mLastUpdateTimeTextView;
-    protected TextView mLatitudeTextView;
-    protected TextView mLongitudeTextView;
-
-    protected final static String KEY_REQUESTING_LOCATION_UPDATES = "requesting-location-updates";
-    protected final static String KEY_LOCATION = "location";
-    protected final static String KEY_LAST_UPDATED_TIME_STRING = "last-updated-time-string";
-
-    // Labels.
-    protected String mLatitudeLabel;
-    protected String mLongitudeLabel;
-    protected String mLastUpdateTimeLabel;
-
-    protected boolean mRequestingLocationUpdates = false;
-
-    String mLastUpdateTime = "";
-
-    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
-    public void onLocationChanged(Location location) {
-        mCurrentLocation = location;
-        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
-        updateUI();
-    }
-    private void updateUI() {
-        mLatitudeTextView.setText(String.valueOf(mCurrentLocation.getLatitude()));
-        mLongitudeTextView.setText(String.valueOf(mCurrentLocation.getLongitude()));
-        mLastUpdateTimeTextView.setText(mLastUpdateTime);
-        Log.d("Map", "in update UI");
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        stopLocationUpdates();
-    }
-    protected void stopLocationUpdates() {
-        LocationServices.FusedLocationApi.removeLocationUpdates(
-                mGoogleApiClient, this);
-    }
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (mGoogleApiClient.isConnected() && !mRequestingLocationUpdates) {
-            startLocationUpdates();
-        }
-    }
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        savedInstanceState.putBoolean(KEY_REQUESTING_LOCATION_UPDATES,
-                mRequestingLocationUpdates);
-        savedInstanceState.putParcelable(KEY_LOCATION, mCurrentLocation);
-        savedInstanceState.putString(KEY_LAST_UPDATED_TIME_STRING, mLastUpdateTime);
-        super.onSaveInstanceState(savedInstanceState);
-    }
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#ff757575"));
         getSupportActionBar().setBackgroundDrawable(colorDrawable);
 
-        mLatitudeText = (TextView) findViewById((R.id.latitude_text));
-        mLongitudeText = (TextView) findViewById((R.id.longitude_text));
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        if(mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
-        }
-        mLastUpdateTime = "";
-        updateValuesFromBundle(savedInstanceState);
-
-        createLocationRequest();
-        buildLocationRequest();
-        checkLocationSettings();
+        askForPermissions();
+        longitudeValueBest = (TextView) findViewById(R.id.longitudeValueBest);
+        latitudeValueBest = (TextView) findViewById(R.id.latitudeValueBest);
+        longitudeValueGPS = (TextView) findViewById(R.id.longitudeValueGPS);
+        latitudeValueGPS = (TextView) findViewById(R.id.latitudeValueGPS);
+        longitudeValueNetwork = (TextView) findViewById(R.id.longitudeValueNetwork);
+        latitudeValueNetwork = (TextView) findViewById(R.id.latitudeValueNetwork);
     }
-    protected void onStart() {
-        mGoogleApiClient.connect();
-        super.onStart();
+    private boolean checkLocation() {
+        if (!isLocationEnabled())
+            showAlert();
+        return isLocationEnabled();
     }
-    protected void onStop() {
-        mGoogleApiClient.disconnect();
-        super.onStop();
+    private boolean isLocationEnabled() {
+        return mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
     }
-    protected void checkLocationSettings() {
-        PendingResult<LocationSettingsResult> result =
-                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient,
-                        mLocationSettingsRequest);
-        result.setResultCallback(this);
+    private void showAlert() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Enable Location")
+                .setMessage("Your Locations Settings is set to 'Off'.\nPlease Enable Location to " +
+                        "use this app")
+                .setPositiveButton("Location Settings", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(myIntent);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    }
+                });
+        dialog.show();
     }
-    protected void buildLocationRequest() {
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(mLocationRequest);
-        mLocationSettingsRequest = builder.build();
-    }
-    @Override
-    public void onConnected(Bundle bundle) {
+    public void toggleGPSUpdates(View view) {
+        if (!checkLocation())
+            return;
+        Button button = (Button) view;
         try {
-            if (mRequestingLocationUpdates) {
-                mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-                if (mLastLocation != null) {
-                    mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
-                    mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
-                }
+            if (button.getText().equals(getResources().getString(R.string.pause))) {
+                mLocationManager.removeUpdates(locationListenerGPS);
+                button.setText(R.string.resume);
+            } else {
+                mLocationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER, 2 * 60 * 1000, 10, locationListenerGPS);
+                button.setText(R.string.pause);
             }
-        } catch (SecurityException ex){
-            Log.d("mapActivity", "failed");
+        }catch (SecurityException ex) {
+            Log.d("mapActivity", "you done fucked the seciruty");
         }
     }
-    protected void createLocationRequest() {
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(10000);
-        mLocationRequest.setFastestInterval(5000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-    }
-    @Override
-    public void onResult(LocationSettingsResult locationSettingsResult) {
-        final Status status = locationSettingsResult.getStatus();
-        switch (status.getStatusCode()) {
-            case LocationSettingsStatusCodes.SUCCESS:
-                startLocationUpdates();
-                break;
-            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-                try {
-                    // Show the dialog by calling startResolutionForResult(), and check the result
-                    // in onActivityResult().
-                    status.startResolutionForResult(mapActivity.this, REQUEST_CHECK_SETTINGS);
-                } catch (IntentSender.SendIntentException e) {
-                    Log.i("Location Error", "PendingIntent unable to execute request.");
-                }
-                break;
-            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                Log.i("Location Error", "Location settings are inadequate, and cannot be fixed here. Dialog " +
-                        "not created.");
-                break;
-        }
-    }
-    protected void startLocationUpdates() {
+    public void toggleBestUpdates(View view) {
+        if(!checkLocation())
+            return;
+        Button button = (Button) view;
         try {
-            LocationServices.FusedLocationApi.requestLocationUpdates(
-                    mGoogleApiClient,
-                    mLocationRequest,
-                    this).setResultCallback(new ResultCallback<Status>() {
+            if(button.getText().equals(getResources().getString(R.string.pause))) {
+                mLocationManager.removeUpdates(locationListenerBest);
+                button.setText(R.string.resume);
+            }
+            else {
+                Criteria criteria = new Criteria();
+                criteria.setAccuracy(Criteria.ACCURACY_FINE);
+                criteria.setAltitudeRequired(false);
+                criteria.setBearingRequired(false);
+                criteria.setCostAllowed(true);
+                criteria.setPowerRequirement(Criteria.POWER_LOW);
+                String provider = mLocationManager.getBestProvider(criteria, true);
+                if(provider != null) {
+                    mLocationManager.requestLocationUpdates(provider, 2 * 60 * 1000, 10, locationListenerBest);
+                    button.setText(R.string.pause);
+                    Toast.makeText(this, "Best Provider is " + provider, Toast.LENGTH_LONG).show();
+                }
+            }  }catch (SecurityException ex) {
+            Log.d("mapActivity", "you done fucked the seciruty");
+        }
+    }
+
+    public void toggleNetworkUpdates(View view) {
+        if (!checkLocation())
+            return;
+        Button button = (Button) view;
+        try {
+            if (button.getText().equals(getResources().getString(R.string.pause))) {
+                mLocationManager.removeUpdates(locationListenerNetwork);
+                button.setText(R.string.resume);
+            } else {
+                mLocationManager.requestLocationUpdates(
+                        LocationManager.NETWORK_PROVIDER, 60 * 1000, 10, locationListenerNetwork);
+                Toast.makeText(this, "Network provider started running", Toast.LENGTH_LONG).show();
+                button.setText(R.string.pause);
+            }
+        } catch (SecurityException ex) {
+            Log.d("mapActivity", "you done fucked the seciruty");
+        }
+    }
+    private final LocationListener locationListenerBest = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            longitudeBest = location.getLongitude();
+            latitudeBest = location.getLatitude();
+
+            runOnUiThread(new Runnable() {
                 @Override
-                public void onResult(Status status) {
-                    mRequestingLocationUpdates = true;
+                public void run() {
+                    longitudeValueBest.setText(longitudeBest + "");
+                    latitudeValueBest.setText(latitudeBest + "");
+                    Toast.makeText(mapActivity.this, "Best Provider update", Toast.LENGTH_SHORT).show();
                 }
             });
-        } catch (SecurityException ex) {
-            Log.d("mapActivity", "failed");
         }
-    }
-    @Override
-    public void onConnectionSuspended(int i) {
 
-    }
-    private void updateValuesFromBundle(Bundle savedInstanceState) {
-        Log.i("Map Activity", "Updating values from bundle");
-        if (savedInstanceState != null) {
-            // Update the value of mRequestingLocationUpdates from the Bundle, and make sure that
-            // the Start Updates and Stop Updates buttons are correctly enabled or disabled.
-            if (savedInstanceState.keySet().contains(KEY_LAST_UPDATED_TIME_STRING)) {
-                mRequestingLocationUpdates = savedInstanceState.getBoolean(KEY_LAST_UPDATED_TIME_STRING);
-            }
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
 
-            // Update the value of mCurrentLocation from the Bundle and update the UI to show the
-            // correct latitude and longitude.
-            if (savedInstanceState.keySet().contains(KEY_LOCATION)) {
-                // Since LOCATION_KEY was found in the Bundle, we can be sure that mCurrentLocation
-                // is not null.
-                mCurrentLocation = savedInstanceState.getParcelable(KEY_LOCATION);
-            }
-
-            // Update the value of mLastUpdateTime from the Bundle and update the UI.
-            if (savedInstanceState.keySet().contains(KEY_LAST_UPDATED_TIME_STRING)) {
-                mLastUpdateTime = savedInstanceState.getString(KEY_LAST_UPDATED_TIME_STRING);
-            }
-            updateUI();
         }
-    }
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
-    }
-    public void onStatusChanged(String provider, int status, Bundle extras) {
+        @Override
+        public void onProviderEnabled(String s) {
 
-    }
-    public void onProviderEnabled(String provider) {
+        }
 
-    }
-    public void onProviderDisabled(String provider) {
+        @Override
+        public void onProviderDisabled(String s) {
 
+        }
+    };
+
+    private final LocationListener locationListenerNetwork = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            longitudeNetwork = location.getLongitude();
+            latitudeNetwork = location.getLatitude();
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    longitudeValueNetwork.setText(longitudeNetwork + "");
+                    latitudeValueNetwork.setText(latitudeNetwork + "");
+                    Toast.makeText(mapActivity.this, "Network Provider update", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+    private final LocationListener locationListenerGPS = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            longitudeGPS = location.getLongitude();
+            latitudeGPS = location.getLatitude();
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    longitudeValueGPS.setText(longitudeGPS + "");
+                    latitudeValueGPS.setText(latitudeGPS + "");
+                    Toast.makeText(mapActivity.this, "GPS Provider update", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+    protected void askForPermissions (){
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION,
+                        android.Manifest.permission.INTERNET}
+                        , 10);
+            }
+            return;
+        }
     }
 }
+
+
+
+
+
